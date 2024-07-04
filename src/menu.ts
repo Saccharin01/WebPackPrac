9da167ecad68bc48575671ac5770578208ae5faf
Saccharin01 <chousik01@gmail.com>
2024-07-04 11:05:32 +0900

    feat | menu.ts

--- src/menu.ts
+++ src/menu.ts
@@ -1,3 +1,17 @@
-const test = "hello,world"
+const root = document.getElementById('root')!
 
-console.log(test)
+const anchorTag = () : string => {
+  let result = `<a href = "#">메뉴</a>`
+  return result
+}
+
+const litag = (children : string) : string => {
+  let result = `<li>${children}</li>`
+  return result
+}
+
+root.innerHTML = `
+<ul>
+  ${litag(anchorTag())}
+</ul>
+`
