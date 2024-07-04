5a0ecdffcca1adb71b46ccdd441ae4eb9a3b7c10
Saccharin01 <chousik01@gmail.com>
2024-07-04 11:40:23 +0900

    err | menu.ts

--- src/menu.ts
+++ src/menu.ts
@@ -1,17 +1,42 @@
 const root = document.getElementById('root')!
 
 const anchorTag = (link : string, textNode : string) : string => {
-  let result = `<a href = "${link}">${textNode}}</a>`
+  let result = `<a href = "${link}">${textNode}</a>`
   return result
 }
-
 const litag = (children : string) : string => {
   let result = `<li>${children}</li>`
   return result
 }
 
+interface BasicData {
+  jaemin : "재민",
+  hoyoung : "호영",
+  yugin : "유진",
+  jiyoon : "지윤"
+}
+
+const basicData : BasicData = {
+  jaemin : "재민",
+  hoyoung : "호영",
+  yugin : "유진",
+  jiyoon : "지윤"
+}
+
+
+
+const assembley = (object : BasicData) : string => {
+  let result = ''
+  for(let key in object){
+    result += litag(anchorTag(`#${key}`,object[key]))
+  }
+  // litag(anchorTag(`#${object.jaemin}`, object.jaemin))
+  return result
+}
+
+
 root.innerHTML = `
 <ul>
-  ${litag(anchorTag("#123", "asdfqwer"))}
+  ${assembley(basicData)}
 </ul>
 `
